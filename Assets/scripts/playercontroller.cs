ec73f452b6ccb6e155abf1c27305c9c9415dd574
freddiecooper <72386496+freddiecooper@users.noreply.github.com>
2021-04-27 22:43:09 +0100

    movement

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-
+    [SerializeField] private LayerMask groundLayerMask;
     public float speed;
     public float upForce;
-    private BoxCollider2D boxCollider2d;
 
+    private bool FacingRight = true;
+    private BoxCollider2D boxCollider2d;
     private Rigidbody2D RB;
+
+    private void Awake()
+    {
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
 
     void Start()
     {
@@ -18,17 +24,28 @@ public class playercontroller : MonoBehaviour
 
     void Update()
     {
-        RB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, RB.velocity.y);
+        float move = Input.GetAxisRaw("Horizontal");
+        RB.velocity = new Vector2( move * speed, RB.velocity.y);
 
         if(IsGrounded() && Input.GetButtonDown("Jump"))
         {
             RB.velocity = new Vector2(RB.velocity.x, upForce);
         }
+
+        if (move > 0 && !FacingRight)
+			{
+				Flip();
+			}
+			else if (move < 0 && FacingRight)
+			{
+				Flip();
+			}
     }
+    
     private bool IsGrounded()
         {
-            float extraHeightText = .01f;
-            RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.down, boxCollider2d.bounds.extents.y + extraHeightText);
+            float extraHeightText = .05f;
+            RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2d.bounds.center, Vector2.down, boxCollider2d.bounds.extents.y + extraHeightText, groundLayerMask);
             Color rayColor;
             if(raycastHit.collider != null)
             {
@@ -41,4 +58,11 @@ public class playercontroller : MonoBehaviour
             Debug.DrawRay(boxCollider2d.bounds.center, Vector2.down * (boxCollider2d.bounds.extents.y + extraHeightText),rayColor);
             return raycastHit.collider != null;
         }
+        
+    private void Flip()
+    {
+        FacingRight = !FacingRight;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
 }
