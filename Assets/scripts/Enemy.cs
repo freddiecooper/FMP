6a4346cce9cfbeff13e8932d07096e763c8d7a43
freddiecooper <72386496+freddiecooper@users.noreply.github.com>
2021-05-03 21:51:34 +0100

    shooting and death

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health = 100;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
